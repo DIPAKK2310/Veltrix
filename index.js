8fb912322bd6ef6e0924df7ff4dcd70fa53d20af
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-24 18:06:06 +0000

    feat:dotenv implement

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hi there from Veltrix");
+import dotenv from "dotenv"
+dotenv.config()
+
+ const Name =  process.env.userName
+ const LastName = process.env.LastName
+console.log(Name,LastName);
