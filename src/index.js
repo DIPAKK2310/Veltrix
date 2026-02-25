33b09a3188481f9c441ae1b97beb24a744b1f62e
DIPAKK2310 <dipakkhare217@gmail.com>
2026-02-25 17:38:22 +0000

    feat:server created using express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from "dotenv"
+import express from "express"
+
+const app = express()
+
 dotenv.config({
     path:"./.env"
 })
- const Profession = "Software Engineer"
- const Name =  process.env.userName
- const LastName = process.env.LastName
-console.log(Name,LastName,":",Profession);
+const port = process.env.PORT //load after dotenv.config() is called
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+app.get('/about',(req,res)=>{
+    res.send("Hello World to about")
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is running on port ${port}`)
+})
+
+
